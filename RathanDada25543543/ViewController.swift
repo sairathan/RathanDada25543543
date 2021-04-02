c291d5cb560518d1fe6c4253fc67e53e77ea1613
sairathan <sairathan885@gmail.com>
2021-04-02 20:12:11 +0530

    Added viewWillAppear in ViewController

--- RathanDada25543543/ViewController.swift
+++ RathanDada25543543/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
 
+    
 }
 
